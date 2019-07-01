556ba6a668e91ef2ffe4a57b3f97f647d1fb15a4
saru <sarumathiayyappan@gmail.com>
2019-07-01 16:03:58 +0530

    Create hunt19.py

---
+++ hunt19.py
@@ -0,0 +1,10 @@
+num=int(input())
+for i in range(2,num+1):
+  if(num%i==0):
+    t=0
+    for s in range(2,i+1):
+      if(i%s==0) and (s!=i):
+          t=1
+          break
+    if(t==0):
+          print(i)
